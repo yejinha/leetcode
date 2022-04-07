2206f69bb7f3fd5656b05f1b796426ff33b2bacc
yejinha <tttc0213@gmail.com>
2022-04-07 23:52:17 +0900

    rotated array version 2

--- Top_interview_questions/Arrays/3_Rotate_Array.java
+++ Top_interview_questions/Arrays/3_Rotate_Array.java
@@ -1,5 +1,6 @@
 class Solution {
     public void rotate(int[] nums, int k) {
+        // 풀이 1
         // 풀이법: 배열 크기보다 회전 수가 많다면  배열 크기보다 같거나 작도록 수정한다. (한 바퀴 돌아서 반영되지 않기에)
         // 회전된 배열에서 인덱스가 원래 배열에서 어떤 인덱스인지 역으로 계산한다. 
    
@@ -29,6 +30,32 @@ class Solution {
         // 원래 배열을 재정렬 해야하기에 회전 후 값 저장한 배열을 복사한다. 
         for(int i=0; i<nums.length; i++){
             nums[i] = rotated[i];
+        }
+
+        //풀이2
+        // 문자열로 만들어 자른 후 이동시켜준다. 
+        // String object 를 계속 만들어야해서 시간과 메모리 많이 소요하는듯. -> 비효율
+        // make int[] array to String  and split them by ","
+        // it takes more time and memory because it has to make new String objects. 
+        k = k%nums.length;
+
+        String str = Arrays.toString(nums);
+        str=str.replaceAll("\\[","").replaceAll("\\]","");
+        String[] rotatedS = str.split(",");
+
+
+        // 회전 후 앞으로 와야하는 것들부터 넣음  put rotated integer first
+        int rotatedIndex = nums.length-k;
+        for(int i = 0; i<k; i++ ){
+            nums[i]=Integer.parseInt(rotatedS[rotatedIndex].trim());
+            ++rotatedIndex;
+        }
+
+        // 회전후 뒤로 밀린것들 넣음 
+        rotatedIndex =0;
+        for(int i=k; i<nums.length; i++){
+            nums[i]=Integer.parseInt(rotatedS[rotatedIndex].trim());
+            ++rotatedIndex;
         }
     }
 }
